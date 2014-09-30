d57b9c4e488d8341991052e2737f426b56dcc4bf
Samar Kamat <samarkamat+github@gmail.com>
2014-09-30 00:19:19 -0400

    ditching string parsing, and switching to array parsing, which is more natural since that is what argv is

--- pyparams.py
+++ pyparams.py
@@ -1,36 +1,83 @@
-import re
-def get_params_from_list(arglist):
-    return get_params_from_str(' '.join(arglist))
+import re       # used for split
+modname = 'pyparams'
+def super_split(string, delim):
+    ret = [i.strip() for i in string.split(delim)]
+    while ret.count(''):
+        ret.remove('')
+    return ret
 
-def get_params_from_str(argstr):
-    if '-' not in argstr:
-        return argstr.split()
-    args = argstr.split('-')[1:];
-    is_full_name = False
+def get_params_from_list(arglist):
     ret = dict()
-    for arg in args:
-        if not arg: is_full_name = True; continue
-        key=''
-        val=''
-        if is_full_name:
-            parts = re.split('=| ', arg.strip(), 1)
-            key = parts[0]
-            val = ' '.join(parts[1:])
-            is_full_name = False
+    ret[''] = []
+    is_key = False
+    look_for_val = False
+    key = ''
+    for arg in arglist:
+        arg = arg.strip()
+        #print ("arg: ", arg)
+        is_key = arg[0] == '-'
+        if is_key:
+            # check if full named or abbreviated
+            #print ('\tkey')
+            if arg[0:2] == '--':
+                #print ("\t\tnamed key")
+                # Named Key
+                arg = arg[2:]
+                argtoks = super_split(arg,'=')
+                key = argtoks[0]
+                if len(argtoks) == 2:
+                    #print ('\t\twith val: ', argtoks[1])
+                    val = argtoks[1]
+                    if ',' in val:
+                        val = super_split(val, ',')
+                    ret[key] = val
+                    key = ''
+                elif len(argtoks) > 2:
+                    print (modname, ":ERROR: Could not parse argument: ", arg)
+                else:
+                    ret[key] = True
+            else:
+                #print ('\t\tabbrev key')
+                # Abbreviated Key
+                arg = arg[1:]
+                argtoks = arg.split('=')
+                is_value_present = len(argtoks) == 2
+                #if is_value_present: 
+                    #print('\t\twith val: ', argtoks[1])
+                for i in argtoks[0][0:-1]:
+                    ret[i] = True
+                last_abbrev_key = argtoks[0][-1]
+                if not is_value_present:
+                    ret[last_abbrev_key] = True
+                else:
+                    val = argtoks[1].strip()
+                    if ',' in val:
+                        valtoks = super_split(val, ',')
+                        ret[last_abbrev_key] = valtoks
+                    else:
+                        ret[last_abbrev_key] = val
+                        #print ('set key: ', key, ' to val: ', val)
         else:
-            key=arg[0].strip()
-            val=arg[1:].strip()
-        if not val:
-            val = True
-        elif ',' in val:
-            val = [i.strip() for i in val.split(',')]
-        ret[key] = val
+            #print ('\tvalue, current key is ', key)
+            # not key
+            if ',' in arg:
+                arg = super_split(arg, ',')
+            if key == '':
+                if isinstance(arg, list):
+                    ret[''].extend(arg)
+                else:
+                    ret[''].append(arg)
+            else:
+                ret[key] = arg
+
+            # reset the key to empty
+            key = ''
     return ret
 
 def get_params_strict (argstr, param_dict):
-    print_param_dict(param_dict)
     return
 
 def get_param_usage_string (param_dict):
     print ("would print param dict here:")
     print (param_dict)
+    return
--- test.py
+++ test.py
@@ -5,13 +5,31 @@ import pyparams
 import unittest
 
 class ParamsTest (unittest.TestCase):
-    def test_full(self):
-        test_str = "--named_flag -f --named_value=5 -v5 --named_list 5,6,7 -l5,6,7"
-        expected_params = {'named_value': '5', 'l': ['5', '6', '7'], 'named_list': ['5', '6', '7'], 'named_flag': True, 'f': True, 'v': '5'}
-        test_params = pyparams.get_params_from_str(test_str)
-        self.assertTrue(test_params == expected_params)
+    def setUp(self):
+        #self.test_str = "--named_flag -f --named_value=5 -v5 --named_list 5,6,7 -l5,6,7"
+        #self.expected_params = {'named_value': '5', 'l': ['5', '6', '7'], 
+        #    'named_list': ['5', '6', '7'], 'named_flag': True, 
+        #    'f': True, 'v': '5'}
+        self.test_argv = ['--named_flag', '--named_value', '5', '--named_list', '4,5,6', 
+            '-f', '-v=5',  '--quote_val', '"This is a quoted value"', 
+            'anon_val', '"anonymous quoted val"',
+            '--named_val_with_equal=5', '--named_list_with_equal=8,9,0']
+        self.expected_params = {'named_flag': True, 'named_value': '5', 
+            'named_list': ['4','5','6'], 
+            'f': True, 'v': '5', 'quote_val': '"This is a quoted value"', 
+            '': ['anon_val', '"anonymous quoted val"'], 'named_val_with_equal': '5',
+            'named_list_with_equal': ['8','9','0']}
+
+
+#    def test_param_str(self):
+#        test_params = pyparams.get_params_from_str(self.test_str)
+#       self.assertTrue(test_params == self.expected_params)
+#
+    def test_param_list(self):
+        test_params = pyparams.get_params_from_list(self.test_argv)
+        #print (test_params)
+        self.assertEqual(test_params, self.expected_params)
+
 
 if __name__ == '__main__':
     unittest.main()
-
-
